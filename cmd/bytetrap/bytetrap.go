5482c5ae0d38f42d1e6549c65686e2c3067a6f4e
Riley L. <riley@e926.de>
2023-10-10 14:27:54 +0200

    cleanup cmd/bytetrap; add cli options; add helptext; add .gitignore

--- cmd/bytetrap/bytetrap.go
+++ cmd/bytetrap/bytetrap.go
@@ -3,18 +3,41 @@ package main
 import (
 	bt "github.com/rileys-trash-can/bytetrap"
 
-	"log"
+	_ "embed"
 	"os"
+	"strconv"
 )
+
+//go:embed help.txt
+var helptext []byte
 
 func main() {
 	if len(os.Args) == 1 {
-		printALot()
+		err := bt.Write(os.Stdout)
+		os.Stdout.Write([]byte(err.Error()))
+		os.Exit(1)
 	}
-}
 
-// does not return
-func printALot() {
-	err := bt.Write(os.Stdout)
-	log.Fatalf("Error: %s", err)
+	if os.Args[1] == "-1" || os.Args[1] == "--one" {
+		os.Stdout.Write([]byte(bt.GetPasta()))
+
+		return
+	}
+
+	if os.Args[1] == "-n" && len(os.Args) == 3 {
+		i, err := strconv.ParseUint(os.Args[2], 10, 64)
+		if err != nil {
+			os.Stdout.Write([]byte(err.Error()))
+			os.Exit(1)
+		}
+
+		for i += 0; i > 0; i-- {
+			os.Stdout.Write([]byte((bt.GetPasta())))
+		}
+
+		return
+	}
+
+	os.Stdout.Write(helptext)
+	os.Exit(1)
 }
